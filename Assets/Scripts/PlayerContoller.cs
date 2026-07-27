a5ddb8958a5a847213ccfa5346bb002f0e536db9
Shashank Tayal <shashanktayal2009@gmail.com>
2026-07-28 00:15:46 +0530

    Adds player forward movemnt.

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we will add player movement.
+        // Forward Motion
+        transform.Translate(0, 0, 1);
+    }
+}
