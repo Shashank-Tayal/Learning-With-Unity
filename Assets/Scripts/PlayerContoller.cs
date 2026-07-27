851f2f8043ff2a0732d6ed086222032e6ff3825e
Shashank Tayal <shashanktayal2009@gmail.com>
2026-07-28 01:51:06 +0530

    assign the input to a vector2

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -7,18 +7,21 @@ public class PlayerContoller : MonoBehaviour
     public float vehicleSpeed = 5.0f; 
     public float turnSpeed;
     public InputAction moveAction;
+    public Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         // This is where we will add player movement.
-        // Forward Motion
+
+        moveInput = moveAction.ReadValue<Vector2>();
+
         transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
