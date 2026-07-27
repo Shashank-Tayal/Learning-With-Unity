207387e3cb59a4e10d1653c65c5d5ad77d405555
Shashank Tayal <shashanktayal2009@gmail.com>
2026-07-28 01:22:13 +0530

    Camera motion is smooth now

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -13,7 +13,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
